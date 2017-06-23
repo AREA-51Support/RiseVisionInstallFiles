9d5007f6f68b12b4107e5eeb89dcdc34847474eb
Tyler Gwin <tyler@gwins.net>
2017-06-23 15:03:24 -0400

    Change restartCount.sh

--- restartCount.sh
+++ restartCount.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+
+
 restarts="$(cat output.txt)"
 if [ $restarts = "0" ]; then
+	mkdir /home/pi/.config/autostart
+	printf "[Desktop Entry]Name=Installer\nExec=/home/pi/rvplayer/2017.06.19.16.12/initial-run.sh\nType=application" >> /hom/pi/.config/autostart/installer.desktop
+
 	echo "1" > output.txt;
 	sudo sed -i -e '$i \/home/pi/restartCount.sh &\n' /etc/rc.local;
 fi
@@ -13,4 +18,5 @@ fi
 if [ $restarts = "2" ]; then
 	rm output.txt
 	sudo sudo sed -i '/restartCount/d' /etc/rc.local
+	rm /home/pi/.config/autostart/installer.desktop
 fi
