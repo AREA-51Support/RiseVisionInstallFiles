56342f814ad94ceab78c679946fe1bb27462507c
Tyler Gwin <tyler@gwins.net>
2017-06-26 09:21:21 -0400

    add print lines

--- restartCount.sh
+++ restartCount.sh
@@ -6,6 +6,7 @@ restarts="$(cat output.txt)"
 if [ $restarts = "0" ]; then
 	mkdir /home/pi/.config/autostart
 	printf "[Desktop Entry]Name=Installer\nExec=/home/pi/rvplayer/2017.06.19.16.12/initial-run.sh\nType=application" >> /home/pi/.config/autostart/installer.desktop
+	echo "Creating Desktop File"
 
 	echo "1" > output.txt;
 	sudo sed -i -e '$i \/home/pi/restartCount.sh &\n' /etc/rc.local;
@@ -13,6 +14,7 @@ fi
 
 if [ $restarts = "1" ]; then
 	echo "2" > output.txt
+	
 fi
 
 if [ $restarts = "2" ]; then
